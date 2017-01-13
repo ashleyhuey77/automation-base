19fdae5c8b89085b54ec0cac1576c7b173710381
Oded Joseph <kalikojo@odmetas-mbp.turner.com>
2017-01-13 12:22:00 -0500

    drag and drop via seleniumadded

--- src/test/unitTests/PageClassTests.java
+++ src/test/unitTests/PageClassTests.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import commonClasses.sharedUtils.*;
