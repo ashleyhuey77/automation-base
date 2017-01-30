71f7d632ce82b265a1732061bdc2f9b4d181118b
Huey, Ashley <ashley.huey@turner.com>
2017-01-30 10:09:32 -0500

    Added fixes

--- src/test/unitTests/AllTests.java
+++ src/test/unitTests/AllTests.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
 @RunWith(Suite.class)
 @SuiteClasses({ PageClassTests.class, UtilsTests.class, SeleniumHelperTests.class })
-public class AllTests {
+ public class AllTests {
+
 
 }
