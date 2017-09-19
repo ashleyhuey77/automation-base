289f3b3e35017c9eda76e8a1d6be40cba52ff6f0
Huey, Ashley <ashley.huey@turner.com>
2017-09-19 13:13:34 -0400

    adding more detailed javadocs to project

--- src/main/commonClasses/sharedUtils/helpers/ReportHelper.java
+++ src/main/commonClasses/sharedUtils/helpers/ReportHelper.java
@@ -14,12 +14,41 @@ public class ReportHelper {
         this.htmlReport = htmlReport;
     }
 
+    /**
+     * <p>This method will report a done event to the test steps html 
+     * report that is generated at the end of each test run.</p>
+     * <p>This method would get called when it is necessary to document
+     * a step to the html report but it is not necessary to have a screencap.</p>
+     * <p>A line will be printed to the html report in the following format:</p>
+     * <p>Step: [stepName] has passed. [description]</p>
+     * <p>The step name param in the above example is autopopulated with
+     * the last step that was executed in the stack trace.</p>
+     * @param description - a description of the step that was executed. The text
+     * one wishes to display on the html report for the executed step.
+     * @throws FrameworkException
+     * @throws IOException
+     */
     public void reportDoneEvent(String description) throws FrameworkException, IOException {
         String stepName = Thread.currentThread().getStackTrace()[2].getMethodName();
         htmlReport.reportDoneEvent(stepName, description);
         System.out.println("Step: " + stepName + " has passed. " + description);
     }
 
+    /**
+     * <p>This method will report an exception message to the test steps html 
+     * report that is generated at the end of each test run.</p>
+     * <p>This method would get called in a catch block in order to capture the
+     * error thrown and report it to the html report.</p>
+     * <p>A line will be printed to the html report that contains the webdriver
+     * exception message of the exception that was thrown. This can be helpful
+     * for debugging or if something failed and it wasn't supposed to.</p>
+     * <p>The step name param is autopopulated with
+     * the last step that was executed in the stack trace.</p>
+     * @param webDriverException - the exception that was thrown during execution of the method.
+     * @return FrameworkException
+     * @throws FrameworkException
+     * @throws IOException
+     */
     public FrameworkException reportException(Exception webDriverException) throws FrameworkException, IOException {
         String stepName = Thread.currentThread().getStackTrace()[2].getMethodName();
         htmlReport.reportFailEvent(stepName, webDriverException.getMessage());
--- src/main/commonClasses/sharedUtils/helpers/ValidationsHelper.java
+++ src/main/commonClasses/sharedUtils/helpers/ValidationsHelper.java
@@ -15,12 +15,40 @@ public class ValidationsHelper {
         this.htmlReport = htmlReport;
     }
 
+    /**
+     * <p>This method will report a pass event to the test steps html 
+     * report that is generated at the end of each test run.</p>
+     * <p>This method would get called when it is necessary to document
+     * a passed step to the html report along with a screencap of the passed event.</p>
+     * <p>A line will be printed to the html report in the following format:</p>
+     * <p>Step: [stepName] has passed. [message]</p>
+     * <p>The step name param in the above example is autopopulated with
+     * the last step that was executed in the stack trace.</p>
+     * @param message - a description of the step that was executed. The text
+     * one wishes to display on the html report for the executed step.
+     * @throws FrameworkException
+     * @throws IOException
+     */
     public void assertionPass(String message) throws FrameworkException, IOException {
         String stepName = Thread.currentThread().getStackTrace()[2].getMethodName();
         htmlReport.reportPassEvent(stepName, message);
         System.out.println("Step: " + stepName + " has passed. " + message);
     }
 
+    /**
+     * <p>This method will report a fail event to the test steps html 
+     * report that is generated at the end of each test run.</p>
+     * <p>This method would get called when it is necessary to document
+     * a failed step to the html report along with a screencap of the failed event.</p>
+     * <p>A line will be printed to the html report in the following format:</p>
+     * <p>StepName: [stepName]. ErrorMessage: [message]</p>
+     * <p>The step name param in the above example is autopopulated with
+     * the last step that was executed in the stack trace.</p>
+     * @param message - a description of the step that was executed. The text
+     * one wishes to display on the html report for the executed step.
+     * @throws FrameworkException
+     * @throws IOException
+     */
     public FrameworkException assertionFailed(String message) throws FrameworkException, IOException {
         String stepName = Thread.currentThread().getStackTrace()[2].getMethodName();
         AssertionFailedError assertionFailedError = getAssertionFailedErrorObject(stepName, message);
@@ -31,7 +59,7 @@ public class ValidationsHelper {
     }
 
     private AssertionFailedError getAssertionFailedErrorObject(String stepName, String errorMessage) {
-        String message = "StepName: " + stepName + "\n ErrorMessage : " + errorMessage;
+        String message = "StepName: " + stepName + "\n ErrorMessage: " + errorMessage;
         AssertionFailedError assertionFailedError = new AssertionFailedError(message);
         return assertionFailedError;
     }
