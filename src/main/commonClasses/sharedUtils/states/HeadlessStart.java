3cd64984120bced547601b3fb02d8fa1a3376ffe
Huey, Ashley <ashley.huey@turner.com>
2017-09-20 10:32:37 -0400

    changed screensize for headless to 1920,1080

--- src/main/commonClasses/sharedUtils/states/HeadlessStart.java
+++ src/main/commonClasses/sharedUtils/states/HeadlessStart.java
@@ -9,6 +9,7 @@ public class HeadlessStart implements State {
 	public void doAction() {
 		LocalChromeOptions.get().addArguments("headless");
 		LocalChromeOptions.get().addArguments("disable-gpu");
+		LocalChromeOptions.get().addArguments("window-size=1920,1080");
 		System.out.println("Headless mode has been enabled. All tests will run in headless Chrome.");
 	}
 
